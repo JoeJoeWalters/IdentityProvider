bb71a50894e4b4ffe8778aa0f3a35b63f76a708f
Joe Walters <info@thenakeddeveloper.com>
2022-07-26 18:11:45 +0100

    fleshing out the jwks object

--- src/Server/Contracts/MetaData/JWKS.cs
+++ src/Server/Contracts/MetaData/JWKS.cs
@@ -1,12 +1,54 @@
 ﻿namespace Server.Contracts.MetaData
 {
+    //https://auth0.com/blog/navigating-rs256-and-jwks/
     public class JWKS
     {
-        public JWKSKeys Keys { get; set; }
+        /// <summary>
+        /// List of available keys for use with the identity provider
+        /// </summary>
+        public List<JWKSKey> Keys { get; set; }
     }
 
-    public class JWKSKeys
+    public class JWKSKey
     {
+        /// <summary>
+        /// algorithm for the key
+        /// </summary>
+        public string alg { get; set; }
 
+        /// <summary>
+        ///  key type
+        /// </summary>
+        public string kty { get; set; }
+
+        /// <summary>
+        /// how the key was meant to be used. e.g. sig represents signature verification.
+        /// </summary>
+        public string use { get; set; } = "sig";
+
+        /// <summary>
+        /// is the x509 certificate chain
+        /// </summary>
+        public string x5c { get; set; }
+
+        /// <summary>
+        /// moduluos for a standard pem
+        /// </summary>
+        public string n { get; set; }
+
+        /// <summary>
+        /// exponent for a standard pem
+        /// </summary>
+        public string e { get; set; }
+
+        /// <summary>
+        /// unique identifier for the key
+        /// </summary>
+        public string kid { get; set; }
+
+        /// <summary>
+        /// thumbprint of the x.509 cert (SHA-1 thumbprint)
+        /// </summary>
+        public string x5t { get; set; }
     }
 }
