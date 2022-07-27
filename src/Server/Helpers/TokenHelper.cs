3b4aed51fbcb62d4c517c0b333de3f0310cc7187
Joe Walters <info@thenakeddeveloper.com>
2022-07-27 22:45:08 +0100

    Placeholders for doing token generation from another token (e.g. token from refresh or refresh from token)

---
+++ src/Server/Helpers/TokenHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace Server.Helpers
+{
+    /// <summary>
+    /// Helpers for generating and manipulating tokens
+    /// </summary>
+    public static class TokenHelper
+    {
+        /// <summary>
+        /// Generate the refresh token from the security token
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public static JwtSecurityToken GenerateRefreshToken(this JwtSecurityToken token)
+        {
+            return new JwtSecurityToken() { }; // Placeholder
+        }
+
+        /// <summary>
+        /// Generate a new security token from the refresh token
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public static JwtSecurityToken GenerateFromRefreshToken(this JwtSecurityToken token)
+        {
+            return new JwtSecurityToken() { }; // Placeholder
+        }
+    }
+}
