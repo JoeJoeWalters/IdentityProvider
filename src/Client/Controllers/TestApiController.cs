7706dc773b75f8c1b5709859a564a6ff828d6375
TNDStudios <info@thenakeddeveloper.com>
2022-08-17 22:55:27 +0100

    Added in test controller that will be used for testing API calls from the client with the token given. so we can test scope checking / ACR / Audience validation / Step up etc.

---
+++ src/Client/Controllers/TestApiController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers
+{
+    /// <summary>
+    /// Controller for testing API calls and the functionality for verifying that the scope of the token
+    /// given and the ACR is correct etc. before proceeding or requesting step up etc.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestApiController : ControllerBase
+    {
+        // GET: api/<TestApiController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<TestApiController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<TestApiController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<TestApiController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<TestApiController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
